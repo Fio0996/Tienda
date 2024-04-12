c002b7f6a2d61bd42da098243da0153c11b70842
Fiorella <127554229+Fio0996@users.noreply.github.com>
2024-04-12 15:46:00 -0700

    finalizacion semana 13

--- src/main/java/com/tienda/controller/IndexController.java
+++ src/main/java/com/tienda/controller/IndexController.java
@@ -1,9 +1,13 @@
 
 package com.tienda.controller;
 
+import com.tienda.domain.Item;
+import com.tienda.service.ItemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  *
@@ -17,6 +21,21 @@ public class IndexController {
         model.addAttribute("attribute", "value");
         return "index";
     }
+    @Autowired   
+    private ItemService itemService;  
+    @RequestMapping("/refrescarBoton")   
+    public ModelAndView refrescarBoton(Model model) {  
+        var lista = itemService.gets();        
+        var totalCarritos = 0;        
+        var carritoTotalVenta = 0;        
+        for (Item i : lista) {      
+            totalCarritos += i.getCantidad();         
+            carritoTotalVenta += (i.getCantidad() * i.getPrecio()); 
+        }       
+        model.addAttribute("listaItems", lista);      
+        model.addAttribute("listaTotal", totalCarritos);       
+        model.addAttribute("carritoTotal", carritoTotalVenta);        
+        return new ModelAndView("/carrito/fragmentos :: verCarrito");    }
 }
 /**
  *
